beedaf478961208313a07726f6cc0e3a37702ee1
GitHub Action <action@github.com>
2026-08-04 13:31:25 +0000

    chore(release): 0.4.2 (2015)

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,8 +75,8 @@ subprojects {
 
             // Product version, independent of the CMFA release we forked from.
             // The mihomo core reports its own version via Bridge.nativeCoreVersion().
-            versionName = "0.4.1"
-            versionCode = 2014
+            versionName = "0.4.2"
+            versionCode = 2015
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
