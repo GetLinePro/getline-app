5fcec30805eb509da9f8ca01d128859df70a8829
GitHub Action <action@github.com>
2026-08-16 07:28:19 +0000

    chore(release): 0.8.2 (2023)

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,8 +75,8 @@ subprojects {
 
             // Product version, independent of the CMFA release we forked from.
             // The mihomo core reports its own version via Bridge.nativeCoreVersion().
-            versionName = "0.8.1"
-            versionCode = 2022
+            versionName = "0.8.2"
+            versionCode = 2023
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
